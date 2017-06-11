ede3cbbee368c9805c8ef72019b2f7e01f7de260
Alexey Slesarev <lexsl84@gmail.com>
2017-06-11 18:55:52 +0700

    add app to github

---
+++ app/controllers/my_page_controller.rb
@@ -0,0 +1,17 @@
+class MyPageController < ApplicationController
+  def index
+  end
+
+  def main
+  end
+
+  def about
+  end
+
+  def portfolio
+  end
+
+  def show
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+resources :my_page
+root :to => "my_page#index"
+
+get "main" => "my_page#main"
+get  'my_page/about'
+get  'my_page/portfolio'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
