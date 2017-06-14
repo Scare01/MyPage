45bd4cb2ba45fdb5afb5418eabb8d6e4d6539902
Alexey Slesarev <lexsl84@gmail.com>
2017-06-14 19:43:32 +0700

    work on boby and content

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ resources :my_page
 root :to => "my_page#index"
 
 get "main" => "my_page#main"
-get  'my_page/about'
+get  "about" => "my_page#about"
 get  'my_page/portfolio'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
