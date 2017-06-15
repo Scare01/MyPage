5a4fdcb4da4aae3e6a1ca24785ba1056cc571322
Alexey Slesarev <lexsl84@gmail.com>
2017-06-15 10:41:57 +0000

    star work on portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ root :to => "my_page#index"
 
 get "main" => "my_page#main"
 get  "about" => "my_page#about"
-get  'my_page/portfolio'
+get  "portfolio" => "my_page#portfolio"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
