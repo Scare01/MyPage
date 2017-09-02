52ea412d5aebfe5d2546e4c55563a6a2f3524567
Alexey Slesarev <lexsl84@gmail.com>
2017-09-02 19:22:47 +0700

    many changes

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module MyPage
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.enable = true
+    config.assets.paths << Rails.root.join('app/assets/fonts')
   end
 end
