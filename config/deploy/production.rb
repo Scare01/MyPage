fa3cb308b53cde95ade5fa71abcef01fd6242192
Alexey Slesarev <lexsl84@gmail.com>
2017-09-11 20:07:50 +0700

    m

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '23.227.163.42', user: 'admin', roles: %w{app db web}
+#server '23.227.163.42', user: 'admin', roles: %w{app db web}
 
 
 # role-based syntax
